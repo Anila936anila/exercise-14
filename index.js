3421941ca320aeb078201bc946b8ea6372b61b2e
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:40:53 -0800

    guest list
    Typescript 45Q assignment exercise 14

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+let guestlist = [
+    "Kamran Khan Tessori",
+    "Zia Khan",
+    "Eeshah Hunain",
+    "Hunain Umer",
+    "Pervez Musharraf",
+    "Pukhraj Begam",
+    "Muhammad Sikandar",
+    "Quaid-e-Azam",
+    "Albert Einstine",
+];
+// Loop for each to print invitation
+guestlist.forEach((guestName) => {
+    console.log(`Dear ${guestName}, you are invited to dinner please join us.`);
+});
